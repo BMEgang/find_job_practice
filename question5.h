4d1cd861d942470a1b25043bb88e748933426fcc
BMEgang <51723878+BMEgang@users.noreply.github.com>
2021-11-28 20:45:34 +0000

    practice
    practice

--- question5.h
+++ question5.h
@@ -3,9 +3,48 @@
 class replaceSpace
 {
 public:
-   void replaceblank()
+    //length is the total capacity of the string array
+   void replaceblank(char string[], int length)
    {
+        if(string == nullptr || length < 0)
+        {
+            return;
+        }
+        /*originallength is the original length of string*/
+        int originallength = 0;
+        int numberofblank = 0;
+        int i = 0;
 
+        while (string[i] != '\0') {
+            ++originallength;
+            if(string[i] == ' ')
+            {
+                ++numberofblank;
+            }
+            ++i;
+        }
+        /*originallength is the original length of string*/
+        int newlength = originallength + 2* numberofblank;
+        if (newlength > length)
+        {
+            return;
+        }
+
+        int indexoforiginal = originallength;
+        int indexofnew = newlength;
+        while (indexoforiginal >= 0 && indexofnew > indexoforiginal) {
+            if(string[indexoforiginal] == ' ')
+            {
+                string[indexofnew--]='0';
+                string[indexofnew--]='2';
+                string[indexofnew--]='%';
+            }
+            else {
+                string[indexofnew--]=string[indexoforiginal];
+            }
+
+            --indexoforiginal;
+        }
    }
 };
 
